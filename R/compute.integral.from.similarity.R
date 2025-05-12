650a225fc15fbfee20749611e49c654db460659f
jessica <jessica.gliozzo@gmail.com>
2025-05-12 10:40:51 +0200

    Fix bug - colMeans instead of mean to compute means across columns of dataframe

--- R/compute.integral.from.similarity.R
+++ R/compute.integral.from.similarity.R
@@ -1,5 +1,6 @@
 "compute.integral.from.similarity" <-
 function(sim.matrix) {
-  return(as.vector(1 - mean(as.data.frame(t(sim.matrix)))));
+  #return(as.vector(1 - mean(as.data.frame(t(sim.matrix)))));
+  return(as.vector(1 - colMeans(as.data.frame(t(sim.matrix)))));
 }
 
